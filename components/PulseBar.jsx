fd34994c3b20fadd352fa671837c6383a91ffcdf
Kumara2mahe <mahendrakumara268@gmail.com>
2024-02-05 22:05:56 +0530

    Add pulsing animations while data fetch

---
+++ components/PulseBar.jsx
@@ -0,0 +1,10 @@
+
+const PulseBar = () => {
+    return (
+        <div id="pulse-bar">
+            <div className="pb_slider first_slider" />
+            <div className="pb_slider second_slider" />
+        </div>
+    )
+}
+export default PulseBar
